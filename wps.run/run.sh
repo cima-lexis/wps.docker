c830011229a63794ae9bfb8ff93e9cc0a9e12e3d
Debian <debian@test-dockers.novalocal>
2020-02-04 15:12:48 +0000

    use all availables cores

--- wps.run/run.sh
+++ wps.run/run.sh
@@ -19,19 +19,25 @@ else
    exit
 fi
 
+# stop on first error
+set -e
+
 # preparing namelist files from templates.
 export start=$1
 export end=$2
 cat namelist.input.tmpl | ./namelist-prepare $start $end > namelist.input
 cat namelist.wps.tmpl | ./namelist-prepare $start $end > namelist.wps
 
+# number of processor cores availables
+export cores=`nproc`
+
 # execute WPS
-mpiexec -n 6 ./geogrid.exe
+mpiexec -n $cores ./geogrid.exe
 ./link_grib.csh /input/*
 ./ungrib.exe
 ./avg_tsfc.exe
-mpiexec -n 6 ./metgrid.exe
-mpiexec -n 6 ./real.exe
+mpiexec -n $cores ./metgrid.exe
+mpiexec -n $cores ./real.exe
 
 # publish output files
 cp wrfbdy_* wrfinput_* /output
