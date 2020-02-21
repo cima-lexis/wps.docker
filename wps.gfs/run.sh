446845c1686218fa947ed20d3e79284450974f02
Andrea Parodi <andrea.parodi@cimafoundation.org>
2020-02-21 07:56:12 +0000

    Fix run script

--- wps.gfs/run.sh
+++ wps.gfs/run.sh
@@ -1,19 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# read arguments from command line
-# or from environment.
-if [ "$#" -eq 2 ]; then
-  export start=$1
-  export end=$2
-else
-  export start=$WPS_START_DATE
-  export end=$WPS_END_DATE
-fi
-
-docker run -it                      \
-    -v $PWD/output:/output          \
-    -v $PWD/../gfs/:/input          \
-    -v /vols/data/geog:/geogrid     \
-    -e WPS_START_DATE=$1            \
-    -e WPS_END_DATE=$2              \
-    wps.gfs sh run.sh
+docker run -it \
+-v $PWD/output:/output \
+-v $PWD/../gfs/:/input \
+-v /vols/data/geog:/geogrid \
+-e "WPS_START_DATE=$1" \
+-e "WPS_END_DATE=$2" \
+wps.gfs bash run.sh
--- wps.run/run.sh
+++ wps.run/run.sh
@@ -25,11 +25,11 @@ set -e
 # read arguments from command line
 # or from environment.
 if [ "$#" -eq 2 ]; then
-  export start = $1
-  export end = $2
+  export start=$1
+  export end=$2
 else
-  export start = $WPS_START_DATE
-  export end = $WPS_END_DATE
+  export start=$WPS_START_DATE
+  export end=$WPS_END_DATE
 fi
 
 # prepares namelist files from templates.
