d7eb68f61c5de6cbe4ab2ae9ae3aeb3cc24bc0b7
Andrea Parodi <andrea@parro.it>
2020-08-19 09:15:56 +0200

    Download gfs (#6)
    * IFS: use namelists specific for france cases
    * gfs download example
    * add arguments in run.sh to specify GFS subfolder to use
    * moved namelist-prepare to https://github.com/meteocima/namelist-prepare
    
    Co-authored-by: Andrea Parodi <andrea.parodi@cimafoundation.org>

--- wps.gfs/run.sh
+++ wps.gfs/run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+outdir=$PWD/output/$GFS_DS
+indir=$PWD/../gfs/$GFS_DS
+
+mkdir -p $outdir
 
 docker run -it \
--v $PWD/output:/output \
--v $PWD/../gfs/:/input \
+-v $outdir:/output \
+-v $indir:/input \
 -v /vols/data/geog:/geogrid \
 -e "WPS_START_DATE=$1" \
 -e "WPS_END_DATE=$2" \
