03cfe3ed2ba02d3360143cbc9e607b488452f123
Andrea Parodi <andrea@parro.it>
2020-08-20 12:32:33 +0200

    add mode argument to choose simulation kind

--- wps.run/run.sh
+++ wps.run/run.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
 
-# check input and environment validity
+# This command execute WPS to pre-process data needed
+# by the various workflow at LEXIS.
+#
+# It require three arguments, specified as environment variables
+# or otherwise as command line arguments. when specified as command line arguments,
+# the order of them should be WPS_START_DATE WPS_END_DATE WPS_MODE
+#
+# Arguments:
+#
+# - WPS_START_DATE: initial date/time of the simulation, in format YYYYMMDDHHNN
+# - WPS_END_DATE: final date/time of the simulation, in format YYYYMMDDHHNN
+# - WPS_MODE: kind of simulation to preprocess. accepts these values: 'WARMUP' 'WRF' 'WRFDA'
+#   * WRF mode - preprocess the data needed to run a WRF simulation without data assimilation.
+#               It's actually used by Continuum and numtech simulation.
+#   * WRFDA mode - preprocess the data needed to run a WRFDA simulation with data assimilation.
+#               It includes inthe preprocessed data instant WPS_START_DATE-3HOUR and WPS_START_DATE-6HOUR, that are
+#               used during data assimilation. It will be used by Continuum and numtech simulations when
+#               data assimilation will be used.
+#   * WARMUP mode - preprocess the data needed to run three WRF simulation, without data assimilation.
+#               Two of this WPS execution preprocess the data to run two WRF simulation for days WPS_START_DATE-1 and WPS_START_DATE-2.
+#               These two set of data are the "warmup" data.
+#               The other WPS execution prepares the normal WRF simulation from $WPS_START_DATE to $WPS_END_DATE
+#               Warmup data it's actually used by Continuum and numtech simulation.
+#   * WARMUPDA mode - preprocess the data needed to run three WRF simulation, with data assimilation.
+#               Two of this WPS execution preprocess the data to run two WRF simulation (with no assimilation) for days WPS_START_DATE-1
+#               and WPS_START_DATE-2.
+#               These two set of data are the "warmup" data.
+#               The other WPS execution prepares the normal WRF simulation (with assimilation) from $WPS_START_DATE to $WPS_END_DATE
+#               Warmup data it's actually used by Continuum and numtech simulation.
 
+# read arguments from command line
+# or from environment.
+if [ "$#" -eq 2 ]; then
+  export wps_start=$1
+  export wps_end=$2
+  export wps_mode=$3
+else
+  export wps_start=$WPS_START_DATE
+  export wps_end=$WPS_END_DATE
+  export wps_mode=$WPS_MODE
+fi
+
+
+# check input and environment validity
 if [ ! -d /input ]; then
     echo "This container expect a volume mounted on /input containing input grib files"
     exit
@@ -74,21 +116,10 @@ function dateadd() {
   date -u '+%Y%m%d%H' -d "${dt:0:4}-${dt:4:2}-${dt:6:2} ${dt:8:2}:00 UTC ${amount}"
 }
 
-# read arguments from command line
-# or from environment.
-# WPS_MODE accepts these values: 'RISICO' 'CONTINUUM' 'ADMS' 'WRFDA'
-if [ "$#" -eq 2 ]; then
-  export wps_start=$1
-  export wps_end=$2
-  export wps_mode=$3
-else
-  export wps_start=$WPS_START_DATE
-  export wps_end=$WPS_END_DATE
-  export wps_mode=$WPS_MODE
-fi
 
-if [[ $WPS_MODE == 'RISICO']]; then
 
+if [[ $WPS_MODE == 'WARMUP']]; then
+  # include warmup data
   warmup1_start=`dateadd ${wps_start} "-2 day"`
   warmup2_start=`dateadd ${wps_start} "-1 day"`
   wrfrun_start=${wps_start}
@@ -103,7 +134,12 @@ if [[ $WPS_MODE == 'RISICO']]; then
   exit 0
 fi
 
-if [[ $WPS_MODE == 'CONTINUUM' || $WPS_MODE == 'ADMS' || $WPS_MODE == 'WRFDA' ]]; then
+if [[ $WPS_MODE == 'WRF' ]]; then
+  run_wps $wps_start $wps_end
+  exit 0
+fi
+
+if [[ $WPS_MODE == 'WRFDA' ]]; then
   # include ($wps_start -3 hours) and
   # ($wps_start -6 hours) that are needed
   # for assimilation.
@@ -112,6 +148,26 @@ if [[ $WPS_MODE == 'CONTINUUM' || $WPS_MODE == 'ADMS' || $WPS_MODE == 'WRFDA' ]]
   exit 0
 fi
 
+if [[ $WPS_MODE == 'WARMUPDA']]; then
+  # include warmup data
+  warmup1_start=`dateadd ${wps_start} "-2 day"`
+  warmup2_start=`dateadd ${wps_start} "-1 day"`
+
+  # include ($wps_start -3 hours) and
+  # ($wps_start -6 hours) that are needed
+  # for assimilation.
+  wrfrun_start=`dateadd ${wps_start} -6 hours"`
+
+  warmup1_end=$warmup2_start
+  warmup2_end=$wrf_run_start
+  wrf_run_end=$wps_end
+
+  run_wps $warmup1_start $warmup1_end
+  run_wps $warmup2_start $warmup2_end
+  run_wps $wrfrun_start $wrf_run_end
+  exit 0
+fi
+
 echo Error: unknown mode '$WPS_MODE'
-echo WPS_MODE accepts these values: 'RISICO' 'CONTINUUM' 'ADMS' 'WRFDA'
+echo WPS_MODE accepts these values: 'WARMUP' 'WRF' 'WARMUPDA' 'WRFDA'
 exit 1
